5f1a95ad4f3b9f8f8fd4ddb8d5cff182da5f3544
Rhea Abayan <rhenayanz@gmail.com>
2022-05-14 19:38:38 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+  },
+
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  
+  });
+  
